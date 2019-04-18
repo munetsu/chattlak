594b997ac172f215c74db51b3ceb70fe8de5aa4b
yumefukuro <yumefukuro@gmail.com>
2019-04-19 08:47:56 +0900

    css

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log('実験');
+// Firebaseとの接続するための変数
+const newPostRef = firebase.database();
+
+$(document).on('click', '#btn', function(){
+    let val = $('input').val();
+    newPostRef.ref().push(val);
+})
+
+
+////////////////////////////////////////////
+// css動的調整
+////////////////////////////////////////////
+// textarea
+$(function() {
+    var $textarea = $('#text');
+    var lineHeight = parseInt($textarea.css('lineHeight'));
+    $textarea.on('input', function(e) {
+      var lines = ($(this).val() + '\n').match(/\n/g).length;
+      $(this).height(lineHeight * lines);
+    });
+  });
